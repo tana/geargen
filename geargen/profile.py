5345da9f4072a64673ac5fda323aff2b9e420164
Satoshi Tanaka <overthere555@gmail.com>
2025-04-03 06:42:41 +0900

    Fix wrong shape of gears with many teeth Fillet does not work in such cases

--- geargen/profile.py
+++ geargen/profile.py
@@ -1,4 +1,4 @@
-from math import pi, sin, cos, tan, acos
+from math import pi, sin, cos, tan, acos, atan2
 from typing import cast
 import cadquery as cq
 
@@ -21,12 +21,12 @@ def involuteProfile(
     teeth: int,
     pressureAngle: float = 20.0,
     rootFillet: float | None = None,
-    internal: bool = False
+    internal: bool = False,
 ) -> cq.Wire:
     """Create an involute gear profile at the center of the XY plane"""
 
     if rootFillet is None:
-        rootFillet = 0.38 * module 
+        rootFillet = 0.38 * module
 
     pressureAngleRad = pressureAngle * pi / 180
     pitchRadius = module * teeth / 2
@@ -40,18 +40,30 @@ def involuteProfile(
     # Assuming tooth width on the pitch circle is half of the pitch
     tipAngle = pi / teeth + 2 * (pitchInvoluteAngle - involuteAngle)
 
+    involuteStartRadius = max(baseRadius, rootRadius)
+    # Angular offset for connecting a root arc and an involute (when rootRadius < baseRadius)
+    (involuteStartX, involuteStartY) = involute(involuteStartRadius, baseRadius, 0, False)
+    involuteStartAngle = atan2(involuteStartY, involuteStartX)
+
     wp = cq.Workplane("XY")
-    wp = wp.moveTo(rootRadius, 0)
+    wp = wp.moveTo(rootRadius * cos(involuteStartAngle), rootRadius * sin(involuteStartAngle))
 
     for i in range(teeth):
         startAngle = 2 * pi * i / teeth
         endAngle = 2 * pi * (i + 1) / teeth
 
+        # Connection between the root and an involute has two cases
+        # https://involutegearsoft.hatenablog.com/entry/2023/09/18/163506
+        if rootRadius < baseRadius:
+            # Parts near the root are approximated by straight line (unless there are many teeth)
+            wp = wp.lineTo(baseRadius * cos(startAngle), baseRadius * sin(startAngle))
         # Draw the outward involute curve
-        wp = wp.lineTo(baseRadius * cos(startAngle), baseRadius * sin(startAngle))
         wp = wp.parametricCurve(
             lambda t: involute(
-                (tipRadius - baseRadius) * t + baseRadius, baseRadius, startAngle, False
+                (tipRadius - involuteStartRadius) * t + involuteStartRadius,
+                baseRadius,
+                startAngle,
+                False,
             ),
             N=16,
             makeWire=False,
@@ -67,7 +79,7 @@ def involuteProfile(
         # Draw the inward involute curve
         wp = wp.parametricCurve(
             lambda t: involute(
-                (tipRadius - baseRadius) * (1 - t) + baseRadius,
+                (tipRadius - involuteStartRadius) * (1 - t) + involuteStartRadius,
                 baseRadius,
                 startAngle + 2 * involuteAngle + tipAngle,
                 True,
@@ -75,25 +87,30 @@ def involuteProfile(
             N=16,
             makeWire=False,
         )
-        wp = wp.lineTo(
-            rootRadius * cos(startAngle + 2 * involuteAngle + tipAngle),
-            rootRadius * sin(startAngle + 2 * involuteAngle + tipAngle),
-        )
+        if rootRadius < baseRadius:
+            wp = wp.lineTo(
+                rootRadius * cos(startAngle + 2 * involuteAngle + tipAngle),
+                rootRadius * sin(startAngle + 2 * involuteAngle + tipAngle),
+            )
         # Draw the root between teeth
         wp = wp.radiusArc(
-            (rootRadius * cos(endAngle), rootRadius * sin(endAngle)), -rootRadius
+            (rootRadius * cos(endAngle + involuteStartAngle), rootRadius * sin(endAngle + involuteStartAngle)), -rootRadius
         )
 
     wp = wp.close()
     wire = cast(cq.Wire, wp.val())
 
-    # TODO: Tip fillet when internal gears (currently fillet2D raises error)
-    if not internal:
+    # TODO: Fillet for gears with many teeths (currently fillet2D raises error)
+    if rootRadius < baseRadius:
         # Fillet only root vertices
         rootVertices = [
-            v for v in wire.Vertices() if v.X * v.X + v.Y * v.Y < baseRadius * baseRadius
+            v
+            for v in wire.Vertices()
+            if v.X * v.X + v.Y * v.Y < (rootRadius * 1.001) ** 2
         ]
         assert len(rootVertices) == 2 * teeth
-        wire = wire.fillet2D(radius=rootFillet, vertices=rootVertices)  # This has to be fillet2D, not fillet
+        wire = wire.fillet2D(
+            radius=0.1, vertices=rootVertices
+        )  # This has to be fillet2D, not fillet
 
     return wire
