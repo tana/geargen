c0a86d23b691307d968cac5caa9a5fff5f1b4125
Satoshi Tanaka <overthere555@gmail.com>
2025-04-05 06:12:10 +0900

    Implement bevel gear

---
+++ geargen/bevel.py
@@ -0,0 +1,83 @@
+from typing import Literal, cast
+from math import pi, sin, cos, tan, atan, sqrt
+import cadquery as cq
+from OCP.BRepBuilderAPI import BRepBuilderAPI_MakeFace  # type: ignore
+from OCP.gp import gp_Sphere  # type: ignore
+from .profile import involuteProfile
+
+
+def makeBevelGear(
+    module: float,
+    teeth: int,
+    counterpartTeeth: int,
+    width: float,
+    shaftAngle: float = 90.0,
+    pressureAngle: float = 20.0,
+    rootFillet: float | None = None,
+    type: Literal["bevel"] = "bevel",
+    flatPartCoeff: float = 0.9,
+) -> cq.Solid:
+    pitchRadius = module * teeth / 2
+    shaftAngleRad = shaftAngle * pi / 180
+    coneAngle = atan(
+        sin(shaftAngleRad) / ((counterpartTeeth / teeth) + cos(shaftAngleRad))
+    )
+    coneDistance = pitchRadius / sin(coneAngle)
+    coneHeight = pitchRadius / tan(coneAngle)
+
+    profile = involuteProfile(
+        module,
+        teeth,
+        pressureAngle=pressureAngle,
+        rootFillet=rootFillet,
+    )
+
+    outerSphere = cq.Solid.makeSphere(coneDistance)
+
+    # Project the profile onto a sphere
+    projected = cast(
+        cq.Wire,
+        profile.translate((0, 0, coneHeight)).project(
+            cast(cq.Face, outerSphere.faces("not %PLANE")), (0, 0, -1), closest=True
+        ),
+    )
+
+    # Create spherical face from the projected Wire
+    gpSphere = gp_Sphere()
+    gpSphere.SetRadius(coneDistance)
+    mf = BRepBuilderAPI_MakeFace(gpSphere, projected.wrapped)
+    outerFace = cq.Face(mf.Face()).fix()
+
+    # Extrude it to make a bevel gear with specified facewidth
+    solid = outerFace.thicken(width)
+
+    innerConeDistance = coneDistance - width
+    innerConeHeight = coneHeight * (innerConeDistance / coneDistance)
+    bottomFlatRadius = flatPartCoeff * pitchRadius
+    topFlatRadius = bottomFlatRadius * innerConeDistance / coneDistance
+    solid = cast(
+        cq.Solid,
+        solid
+        # Add top spherical part
+        + cq.Solid.makeCylinder(
+            innerConeDistance,
+            coneHeight - innerConeHeight,
+            pnt=(
+                0,
+                0,
+                sqrt(innerConeDistance**2 - topFlatRadius) + (coneHeight - innerConeHeight),
+            )
+        )
+        # Remove bottom spherical part
+        - cq.Solid.makeCylinder(
+            coneDistance,
+            coneDistance,
+            pnt=(
+                0,
+                0,
+                sqrt(coneDistance**2 - bottomFlatRadius**2) + coneDistance / 2,
+            ),
+        ),
+    )
+
+    return solid
--- geargen/cli.py
+++ geargen/cli.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import cadquery as cq
 from .parallel import makeParallelShaftGear
+from .bevel import makeBevelGear
 
 
 def main():
@@ -16,6 +17,8 @@ def main():
     argParser.add_argument("-t", "--type", default="spur")
     argParser.add_argument("--normal", action="store_true")
     argParser.add_argument("-od", "--outer_diameter", type=float)
+    argParser.add_argument("-nc", "--counterpart_teeth", type=int)
+    argParser.add_argument("-sa", "--shaft_angle", type=float, default=90)
 
     args = argParser.parse_args()
 
@@ -47,6 +50,17 @@ def main():
             helixAngle=args.helix_angle,
             normal=args.normal,
             outerDiameter=args.outer_diameter,
+        )
+    elif args.type == "bevel":
+        result = makeBevelGear(
+            args.module,
+            args.teeth,
+            args.counterpart_teeth,
+            args.width,
+            shaftAngle=args.shaft_angle,
+            pressureAngle=args.pressure_angle,
+            rootFillet=args.root_fillet,
+            type=args.type,
         )
     else:
         print("Unsupported gear type", file=sys.stderr)
